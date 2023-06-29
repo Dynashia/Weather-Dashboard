a7eb9e4ed14cc0e2dcaee0408f25b516cb2c0b5c
Dynashia Lewis <dlewis0985@yahoo.com>
2023-06-29 19:25:51 -0400

    js file outside of assets folder

---
+++ assets/script.js
@@ -0,0 +1,21 @@
+var cityInput = documents.querySelector("city-input");
+var searchbtn = document.querySelector("searchbtn");
+var currentCity = document.querySelector("currentCity");
+var temp = document.querySelector("temp");
+var history = document.querySelector("history");
+
+var apiKey = "7e2a5e781f1c20e89e41452c5fad460b";
+
+
+
+var getWeatherDetails = (cityName, lat, lon) 
+"http://api.weatherstack.com/current"
+
+let futureWeather = {
+    apiKey: "7e2a5e781f1c20e89e41452c5fad460b",
+
+}
+
+getWeather();
+
+
